7e7c31babb0be218a0b6f136e17fc8885e7522f6
Chris <140223696+cmart1109@users.noreply.github.com>
2026-03-25 22:07:15 -0600

    fx2

--- nextjs-dashboard/app/login/page.tsx
+++ nextjs-dashboard/app/login/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import { Suspense } from 'react';
  
 export default function LoginPage() {
   return (
@@ -11,7 +12,9 @@ export default function LoginPage() {
           </div>
         </div>
         
-          <LoginForm />
+          <Suspense fallback={<div>Loading...</div>}>
+            <LoginForm />
+          </Suspense>
         
       </div>
     </main>
